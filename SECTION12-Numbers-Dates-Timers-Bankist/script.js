c26e78f5ad94b83cfea778ac4325109ae1c39131
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-16 01:04:27 +0000

    The remainder operator

--- SECTION12-Numbers-Dates-Timers-Bankist/script.js
+++ SECTION12-Numbers-Dates-Timers-Bankist/script.js
@@ -285,7 +285,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23.0));
 */
 
-console.log(Math.sqrt(25));
+/*console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
 
@@ -303,8 +303,8 @@ const randomInt = (min, max) =>
 //console.log(randomInt(10, 20));
 
 // rounding integers
-
-console.log(Math.round(23.9));
+*/
+/*console.log(Math.round(23.9));
 
 console.log(Math.ceil(23.3));
 console.log(Math.ceil(23.9));
@@ -321,3 +321,29 @@ console.log(Math.floor(-23.3));
 console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
+*/
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});

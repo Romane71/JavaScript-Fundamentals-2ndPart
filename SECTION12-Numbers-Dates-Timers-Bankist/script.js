34afbbfbf90f3e389f0309d7b46b488db5ed1911
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-16 01:26:47 +0000

    numeric separators

--- SECTION12-Numbers-Dates-Timers-Bankist/script.js
+++ SECTION12-Numbers-Dates-Timers-Bankist/script.js
@@ -323,7 +323,7 @@ console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
 */
 
-console.log(5 % 2);
+/*console.log(5 % 2);
 console.log(5 / 2); // 5 = 2 * 2 + 1
 console.log(8 % 3);
 console.log(8 / 3);
@@ -347,3 +347,19 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+*/
+//
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230000'));
+console.log(parseInt('230_000'));

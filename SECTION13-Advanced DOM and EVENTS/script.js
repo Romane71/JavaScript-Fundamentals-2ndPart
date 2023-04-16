233a06cd01b81a59c36617226363f7b6fb9450a3
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-16 23:26:57 +0000

    styles, attributes and classes

--- SECTION13-Advanced DOM and EVENTS/script.js
+++ SECTION13-Advanced DOM and EVENTS/script.js
@@ -73,3 +73,49 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+// styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+logo.getAttribute('src');
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href);
+console.log(link.getAtttribute('href'));
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Do not use
+logo.className = 'Romane';

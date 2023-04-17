7c08f21fc8c24f7b2d3701dca65a95573000b914
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-17 01:08:04 +0000

    event propagation in practice

--- SECTION13-Advanced DOM and EVENTS/script.js
+++ SECTION13-Advanced DOM and EVENTS/script.js
@@ -165,3 +165,37 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 //h1.onmouseenter = function (e) {
 // alert('onmouseenter: Great! You are reading the heading');
 //};
+
+// event propagation in practice
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, 
+ ${randomInt(0, 255)},
+  ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+// stop propagation
+//e.stopPropagation();
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  false
+);

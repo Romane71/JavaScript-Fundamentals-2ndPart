adb2a16976b0d7025fa9f5ff3b981fe9bb9d0313
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-22 02:19:59 +0000

    Implementing a sticky navigation: the scroll event

--- SECTION13-Advanced DOM and EVENTS/script.js
+++ SECTION13-Advanced DOM and EVENTS/script.js
@@ -7,6 +7,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+//const btnScrollTo = document.querySelector('.btn--scroll-to');
+//const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+//const tabs = document.querySelectorAll('.operations__tab');
+//const tabsContainer = document.querySelector('.operations__tab-container');
+//const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -217,11 +223,19 @@ const handlerHover = function (e) {
     });
     logo.style.opacity = this;
   }
-};
-// Passing 'argument' into handler
-nav.addEventListener('mouseover', handlerHover.blind(0.5));
+  // Passing 'argument' into handler
+  nav.addEventListener('mouseover', handlerHover.blind(0.5));
 
-nav.addEventListener('mouseout', handlerHover.blind(1));
+  nav.addEventListener('mouseout', handlerHover.blind(1));
+};
+
+// Sticky navigation
+
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
 
 // Types of events and event handlers
 

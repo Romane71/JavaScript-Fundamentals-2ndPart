77d47b560597c06514f648d0329c600d6ca32222
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-23 22:32:30 +0000

    Coding challenge1-Section14

---
+++ SECTION14-OOP-WITH-JS/codingchallenge1.js
@@ -0,0 +1,47 @@
+'use strict';
+
+//Instructions
+/*1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+'speed' property. The 'speed' property is the current speed of the car in
+km/h
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 2: 'Mercedes' going at 95 km/h
+*/
+
+// My inputs
+
+// Step 1 // constructor function
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// Step 2
+
+Car.prototype.accelerate = function () {
+  this.speed + 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+// Step 4
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes, 95');
+
+//Step 2/3/4
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.accelerate();

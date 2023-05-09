a3a2d14fe1f81bf5abd724b73768d15ae36843b5
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-09 15:11:47 +0000

    inheritance between classes: constructor functions

--- SECTION14-OOP-WITH-JS/script.js
+++ SECTION14-OOP-WITH-JS/script.js
@@ -170,3 +170,39 @@ console.log(steven.__proto__ === PersonProto);
 const alina = Object.create(PersonProto);
 alina.init('Alina', 1994);
 alina.calcAge();
+
+// Inheritance between 'Classes': Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and  I study ${this.course}`);
+};
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+
+Student.prototype.constructor = Student;
+
+console.dir(Student.prototype.constructor);

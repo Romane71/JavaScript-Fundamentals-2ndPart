a63c51fd259c75eba5a5818adb328da4eabce7ab
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-05-15 13:36:27 +0000

    encapsulation: protected properties and methods

--- SECTION14-OOP-WITH-JS/script.js
+++ SECTION14-OOP-WITH-JS/script.js
@@ -167,9 +167,9 @@ console.log(account.movements);
 
 //console.log(steven.__proto__ === PersonProto);
 
-const alina = Object.create(PersonProto);
-alina.init('Alina', 1994);
-alina.calcAge();
+//const alina = Object.create(PersonProto);
+//alina.init('Alina', 1994);
+//alina.calcAge();
 
 // Inheritance between 'Classes': Constructor Functions
 
@@ -229,3 +229,53 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+
+// Another class example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    //Protected property
+    this._pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  _approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+const acc1 = new Account('Romane', 'EUR', 1111);
+
+//acc1._movements.push(250);
+//acc1._movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1.pin);
